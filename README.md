eb6bcbc5ee329a8b626e230f3313f3ec6bfeb759
NdekoCode <arickbulakali@gmail.com>
2022-03-25 20:44:51 +0200

    Begin personnal Code from Scratch With flutter

--- README.md
+++ README.md
@@ -1,16 +1,23 @@
 # learn_flutter
 
-A new Flutter project.
+## Les conteneurs
 
-## Getting Started
+Les conteneurs c'est une petite boite où on va pouvoir mettre des bouttons, des textes,... à l'interieur
+Ici on va voir deux conteneurs, `les colonnes` et `les lignes`.
 
-This project is a starting point for a Flutter application.
+## StatelessWidget VS StatefulWidget
 
-A few resources to get you started if this is your first Flutter project:
+- StatelessWidget: C'est quelque chose qui ne va jamais changer, càd souvent on les utilisent pour quelque chose qui n'est pas amener à interagir avec l'utilisateur donc qui ne va pas amener à etre modifier tout au long de votre application, par exemple: le nom de votre application, il ne va jamais changer tout au long de votre application
+- StatefulWidget: C'est quelque chose qui changer etre amener à changer au cours de notre application, souvent ils sont amener à interagir avec l'utilisateur
 
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+## Les Widget
 
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+1. Scaffold: Permet d'implementer les choses basic du material design, donc avoir toute la structure d'un projet material design
+Souvent un Scaffold il a:
+
+- Une appBar: est la partie qui est en haut, consider un peut comme la navbar en HTML/CSS
+- Un body: Le corps de l'application
+- Un floatingActionButton: Un boutton flottant
+
+2. Center: Permet de centrer les elements qu'elle contient
+3. Text: Permet de mettre du texte et le personnaliser de maniere à l'afficher à l'écran
---
+++ lib/details.dart
@@ -0,0 +1,111 @@
+// On import les outils de flutter
+import 'package:flutter/material.dart';
+
+// La methode d'entrer de notre application qui retourne runApp(La classe à lancer) qui permet de lancer notre application et l'afficher de facon graphique à l'ecran
+void main() {
+  // MyApp Represente la porte d'entrer de mon application
+  runApp(MyApp());
+}
+
+// La classe qui modelise mon application
+// Toute les classes de flutter herite de widget, ils s'empile les uns sur les autres
+// Ici on voit que MyApp HERITE d'un StatelessWidget cela veut dire que toutes les données à l'interieur ne seront plus modifiable au court de l'application
+class MyApp extends StatelessWidget {
+  // On réecrit la methode "build" de la classe parente qui est `widget` et cette methode "build" nous retourne un Widget sachant que toutes les classe de flutter sont des widget ca sera donc une classe en particulier
+// La methode build ici nous aide à savoir comment est-ce que on va construire cette Application
+// ICI on definit les elements principaux de mon application
+  @override
+  Widget build(BuildContext context) {
+    // MaterialApp est un Widget qui Modelise une application
+    return MaterialApp(
+      // Le titre ou le nom de l'application
+      title: 'App Flutter',
+      // Permet d'afficher le bannier rouge "Debug"
+      debugShowCheckedModeBanner: false,
+      // Le theme de notre application, dans notre cas c'est la couleur blue mais on peux la changer en modifiant la valeur de "primarySwatch" à une autre couleur
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      // Home represente la premiere page de mon application dans notre cas c'est la page d'acceuil, cette application n'a qu'une seule page et cette page est representer par la classe MyHomePage
+      home: MyHomePage(
+          // Le titre de la page d'acceuil ce titre peut varier selon les pages
+          title: 'My Flutter App'),
+    );
+  }
+}
+
+// Une classe qui herite d'un StateFulWidget est dont les données peuvent etre modifier au cours de l'application
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+// Quand on créer une page dynamique on doit avoir un Etat et l'interieur de la page est gerer dans les etats (Les states)
+// On creer alors un Etat pour notre page D'acceuil
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+// On creer la classe representant l'Etat de notre page D'acceuil et cette classe aura une visibilité en privé
+class _MyHomePageState extends State<MyHomePage> {
+  // Une variable privée
+  int _counter = 0;
+// Une methode à l'interieur de l'Etat, pour modifier une variable d'Etat elle doit faire appel à la fonction setState() dans laquel on a le droit maintenant de modifier une variable de state
+// Le but de cette fonction est d'incrementer la variable _counter
+  void _incrementCounter() {
+    setState(() {
+      _counter += 2;
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      _counter -= 2;
+    });
+  }
+
+// La methode build ici nous aide à savoir comment est-ce que on va construire cette page d'acceuil
+  @override
+  Widget build(BuildContext context) {
+    // Le scaffold est un peu considerer comme l'arbre HTML, un scaffold est une page
+    // Ici on demande la page elle est composer de quoi ?
+    return Scaffold(
+      // appBar est un peu considerer comme Navbar
+      appBar: AppBar(
+        // Le titre  à mettre dans la navbar widget.title fait reference à la variable title de MyHomePage
+        title: Text(widget.title),
+      ),
+      // Le corps de notre application comme la partie main
+      // Dans notre cas on dit que le body est entrer
+      body: Center(
+        // Le body prend un seul enfant qui est une colonne à fin d'afficher tous les elements les uns en dessous des autres
+        child: Column(
+          // Permet de positionner tous ce qui se trouve à l'interieur de la colonne, dans notre cas MainAxisAlignment vient de centrer tous ce qui se trouve à l'interieur de la colonne
+          mainAxisAlignment: MainAxisAlignment.center,
+          // Apres avoir positionner ma colonne Ici on demande qu'est-ce qui va y avoir à l'interieur de ma colonne ?
+          // Et donc on va lui filer une liste des classes, une liste des Widget donc
+          children: <Widget>[
+            // Un widget de type de texte qui represente un texte à afficher
+            Text(
+              'Vous avez appuyer sur le bouton plusieurs fois:',
+            ),
+            // Une autre classe Widget qui lui va afficher la valeur de la variable _counter avec un style particulier
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      // Un boutton flottant avec une classe FloatingActionButton qui lui permet de lui donner une fonction lui permettant d'incrementer le compter
+      floatingActionButton: FloatingActionButton(
+        // Quand on presse sur le boutton flottant on execute La fonction pour _incrementCounter qui est declarer dans _MyHomePageState
+        onPressed: _incrementCounter,
+        // Quand on passe sur le boutton on affiche le texte 'Increment' dans une bulle
+        tooltip: 'Ajouter',
+        // Ce boutton aura comme enfant une Icon de type 'Ajout'
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
